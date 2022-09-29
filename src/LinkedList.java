969906094ab6dbf796f3915fd85d6874b48dd9c8
SilentDreamz <ddeep21@vt.edu>
2022-09-29 18:02:44 -0400

    Created a inner Node class

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,4 +1,45 @@
+import list.LinkedList.Node;
 
 public class LinkedList {
 
+    private class Node { // Note: We only need int data, would we need to be
+                         // generic at all like use string data
+
+        private int data;
+        private Node next; 
+
+        private Node(int data, Node nextNode) {
+            
+            this.data = data;
+            next = nextNode;
+            
+        }
+
+        private int getData() {
+            
+            return data;
+            
+        } 
+
+
+        private void setData(int newData) {
+            
+            data = newData;
+            
+        } 
+
+
+        private Node getNext() {
+            
+            return next;
+            
+        }
+
+
+        private void setNext(Node nextNode) {
+            
+            next = nextNode;
+            
+        }
+    }
 }
