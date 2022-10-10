b9c131cb2736072db42ef9693a0a7cb99c426a28
SilentDreamz <ddeep21@vt.edu>
2022-10-10 14:13:40 -0400

    Start of setup for milestone 2

--- src/BigNumArithmetic.java
+++ src/BigNumArithmetic.java
@@ -24,6 +24,9 @@ import java.util.Scanner;
 //  Name: Enk Naran, PID: enknaran
 public class BigNumArithmetic {
 
+    LinkedStack stck;
+    LinkedList list;
+    
     public static void main(String[] args) throws FileNotFoundException {
         if (args.length != 1) {
             throw new IllegalArgumentException(
@@ -45,10 +48,13 @@ public class BigNumArithmetic {
 
     public BigNumArithmetic() {
         // If I had any fields, I'd initialize them here
-        // My note: is this needed?
+        stck = new LinkedStack();
+        
+        
     }
     
     // Note: the printCalculations() method was here before but is 
     // moved to PrintCalculations class for a separate parser implementation
+    
     
 }
