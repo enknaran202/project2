be6489b393d4889afe5c036741d944b648eed637
enknaran202 <enknaran202@gmail.com>
2022-10-14 18:39:42 -0400

    Mult Done

--- src/PrintCalculations.java
+++ src/PrintCalculations.java
@@ -7,14 +7,16 @@ import java.io.IOException;
  * 
  * 
  */
-public class PrintCalculations {
+public class PrintCalculations
+{
     private String fileName;
 
     /**
      * 
      * 
      */
-    public PrintCalculations(String path) {
+    public PrintCalculations(String path)
+    {
 
         fileName = path;
 
@@ -24,19 +26,18 @@ public class PrintCalculations {
     // Note: This is presuming that the Linked List data structure is putting in
     // the number nodes backwards like instead of 1234
     // it would be 4321
-    public LinkedList addition(LinkedList fir, LinkedList sec) { // we check for
-                                                                 // leading
-                                                                 // zeros before
-                                                                 // calling this
-                                                                 // method
+    public LinkedList addition(LinkedList fir, LinkedList sec)
+    {
+        // we check for leading zeros before calling this method
         LinkedList result = new LinkedList();
         LinkedList carry = new LinkedList();
         int counter = 0;
         int added = 0;
 
-        if (!fir.isEmpty() && !sec.isEmpty()) { // Is this necessary and can
-                                                // this be tested accordingly
-                                                // without null issues?
+        if (!fir.isEmpty() && !sec.isEmpty())
+        { // Is this necessary and can
+          // this be tested accordingly
+          // without null issues?
 
             return null;
 
@@ -45,21 +46,26 @@ public class PrintCalculations {
         // Next part is to figure out how to traverse both lists at the same
         // time and identify when either or has "ran out" before the other
         // Should i create a seperate structure to "hold carries?"
-        while (counter <= fir.getSize() || counter <= sec.getSize()) {
+        while (counter <= fir.getSize() || counter <= sec.getSize())
+        {
 
             added = 0; // reset for each single digit addition
 
-            if (counter == fir.getSize()) {
+            if (counter == fir.getSize())
+            {
 
-                while (counter != sec.getSize()) {
+                while (counter != sec.getSize())
+                {
 
                     added = sec.next();
 
-                    if (!carry.isEmpty()) {
+                    if (!carry.isEmpty())
+                    {
 
                         added = added + 1;
                         carry.clear();
-                        if (added == 10) {
+                        if (added == 10)
+                        {
 
                             result.addLow(added - 10);
                             carry.addLow(1); // structure to carry ones, need a
@@ -77,17 +83,21 @@ public class PrintCalculations {
 
             }
 
-            else if (counter == sec.getSize()) {
+            else if (counter == sec.getSize())
+            {
 
-                while (counter != fir.getSize()) {
+                while (counter != fir.getSize())
+                {
 
                     added = fir.next();
-                    
-                    if (!carry.isEmpty()) {
+
+                    if (!carry.isEmpty())
+                    {
 
                         added = added + 1;
                         carry.clear();
-                        if (added == 10) {
+                        if (added == 10)
+                        {
 
                             result.addLow(added - 10);
                             carry.addLow(1); // structure to carry ones, need a
@@ -104,17 +114,20 @@ public class PrintCalculations {
 
             }
 
-            else {
+            else
+            {
 
                 added = fir.next() + sec.next();
-                if (!carry.isEmpty()) {
+                if (!carry.isEmpty())
+                {
 
                     added = added + 1;
                     carry.clear();
 
                 }
 
-                if (added >= 10) {
+                if (added >= 10)
+                {
 
                     result.addLow(added - 10);
                     carry.addLow(1); // structure to carry ones, need a way to
@@ -124,7 +137,8 @@ public class PrintCalculations {
                     // covered but check?)
                 }
 
-                else {
+                else
+                {
 
                     result.addLow(added);
 
@@ -138,6 +152,7 @@ public class PrintCalculations {
         return result;
 
     }
+
 
     /**
      * 
@@ -153,9 +168,11 @@ public class PrintCalculations {
         int curNum1;
         int multiplied = 0;
         int carry = 0;
+        LinkedList addTemp1 = null;
+        LinkedList addTemp2 = null;
         LinkedStack saveMults = new LinkedStack();
         // gets the current number to multiply with the first list
-        for(int i = 0; i < num2.getSize(); i++)
+        for (int i = 0; i < num2.getSize(); i++)
         {
             // Create a new List to save each multiplication
             toReturn = new LinkedList();
@@ -165,15 +182,16 @@ public class PrintCalculations {
             // saves the cur number
             curNum2 = num2.next();
             // Adds zeros if we are using higher power numbers
-            for(int k = 0; k < i; k++)
+            for (int k = 0; k < i; k++)
             {
                 toReturn.addHigh(0);
             }
-            for(int j = 0; j < num1.getSize(); j++)
+            for (int j = 0; j < num1.getSize(); j++)
             {
                 curNum1 = num1.next();
                 multiplied = (curNum1 * curNum2) + carry;
-                // if the multiplication results in a number that needs to be carried
+                // if the multiplication results in a number that needs to be
+                // carried
                 if (multiplied < 9)
                 {
                     // add the first digit to the list
@@ -189,46 +207,62 @@ public class PrintCalculations {
             // save the resulting list in a stack to be added later
             saveMults.push(toReturn);
         }
-        
+
+        while (saveMults.size() > 1)
+        {
+            addTemp1 = saveMults.pop();
+            addTemp2 = saveMults.pop();
+            saveMults.push(addition(addTemp1, addTemp2));
+        }
         // add all the numbers in this stack together
+        toReturn = saveMults.pop();
         return toReturn;
     }
+
 
     /**
      * 
      * 
      */
-    public void printCalculations() throws FileNotFoundException {
+    public void printCalculations() throws FileNotFoundException
+    {
 
         Scanner scanIn = new Scanner(new File(fileName));
         LinkedList tempNum1 = null;
         LinkedList tempNum2 = null;
         LinkedStack stack = new LinkedStack();
 
-        while (scanIn.hasNext()) {
+        while (scanIn.hasNext())
+        {
             String line = scanIn.nextLine();
             if (!line.isEmpty() || !line.trim().equals("") || !line.trim()
-                .equals("\n")) { // possible fix to lines with spaces being
-                                 // included
-                                 // takes out extra spaces between strings
+                .equals("\n"))
+            { // possible fix to lines with spaces being
+              // included
+              // takes out extra spaces between strings
                 line = line.trim().replaceAll(" +", " ");
                 String[] rpn = line.split(" ");
                 // Send the array to a method to ensure correct # of numbers and
-                if (isValid(rpn)) {
-                    for (int i = 0; i < rpn.length; i++) {
+                if (isValid(rpn))
+                {
+                    for (int i = 0; i < rpn.length; i++)
+                    {
                         // if the string is a number
                         // put in the linked list
                         // and push the LL onto the stack
-                        if (Character.isDigit(rpn[i].charAt(0))) {
+                        if (Character.isDigit(rpn[i].charAt(0)))
+                        {
                             tempNum1 = new LinkedList(rpn[i]);
                             stack.push(tempNum1);
                         }
                         // if the string is a command
-                        else {
+                        else
+                        {
                             tempNum1 = stack.pop();
                             tempNum2 = stack.pop();
 
-                            switch (rpn[i]) {
+                            switch (rpn[i])
+                            {
                                 case "+":
                                     // add tempNum1 and tempNum2
                                     // push onto stack
@@ -251,7 +285,8 @@ public class PrintCalculations {
                         }
                     }
                 }
-                else { // if it does not fit RPN print with only =
+                else
+                { // if it does not fit RPN print with only =
                     System.out.println(line + " =");
                 }
             }
@@ -268,20 +303,26 @@ public class PrintCalculations {
      *         is or is not proper
      * 
      */
-    public boolean isValid(String[] input) {
+    public boolean isValid(String[] input)
+    {
         int stackSize = 0;
-        for (int i = 0; i < input.length; i++) {
-            if (Character.isDigit(input[i].charAt(0))) {
+        for (int i = 0; i < input.length; i++)
+        {
+            if (Character.isDigit(input[i].charAt(0)))
+            {
                 stackSize++;
             }
-            else {
+            else
+            {
                 stackSize--;
             }
-            if (stackSize <= 0) {
+            if (stackSize <= 0)
+            {
                 return false;
             }
         }
-        if (stackSize == 1) {
+        if (stackSize == 1)
+        {
             return true;
         }
         return false;
